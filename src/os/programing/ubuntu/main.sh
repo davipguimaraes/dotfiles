1507190e0c39882539fad4baf45e30d7811e8b9f
Davi P. Guimarães <davipuiaraes.dev@gmail.com>
2020-02-26 16:02:42 -0300

    update

--- src/os/programing/ubuntu/main.sh
+++ src/os/programing/ubuntu/main.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")" \
-    && . "../../install/ubuntu/utils.sh" \
-    && . "./"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-update
-upgrade
 
 ./asdf_plugins.sh
 ./php.sh
