755787dac0878b95e2741ba98ddca93d242979a4
Davi P. Guimarães <davipuiaraes.dev@gmail.com>
2020-02-26 15:29:46 -0300

    update scripts asdf and work

--- src/os/install/ubuntu/asdf.sh
+++ src/os/install/ubuntu/asdf.sh
@@ -12,36 +12,8 @@ install_asdf() {
     execute \
         "git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.7.6" \
         "$1"
-    execute "$HOME/.asdf/asdf.sh" "asdf"
-    execute "$HOME/.asdf/completions/asdf.bash" "asdf"
 }
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-install_asdf_plugin() {
-    declare -r EXTRA_ARGUMENTS="$3"
-    declare -r PACKAGE="$2"
-    declare -r PACKAGE_READABLE_NAME="$1"
-
-    print_in_green "$PACKAGE_READABLE_NAME"
-    execute \
-        "asdf plugin-add $PACKAGE $EXTRA_ARGUMENTS"
-
-}
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-install_asdf_add_version() {
-    declare -r EXTRA_ARGUMENTS="$4"
-    declare -r VERSION="$3"
-    declare -r PACKAGE="$2"
-    declare -r PACKAGE_READABLE_NAME="$1"
-
-    execute \
-        "$EXTRA_ARGUMENTS asdf install $PACKAGE $VERSION" \
-        "$PACKAGE_READABLE_NAME" \
-        "asdf global $PACKAGE $VERSION"
-
-}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -63,18 +35,6 @@ main() {
     install_package "ASDF - unixodbc-dev" "unixodbc-dev"
     install_package "ASDF - unzip" "unzip"
     install_package "ASDF - curl" "curl"
-
-    print_in_purple "\n   asdf plugins \n\n"
-
-    install_asdf_plugin "asdf-vm/nodejs" "nodejs" "https://github.com/asdf-vm/asdf-nodejs.git"
-    execute "bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring"
-    install_asdf_add_version "node@12.16.1" "nodejs" "12.16.1"
-
-    install_asdf_plugin "asdf-community/php" "php" "https://github.com/asdf-community/asdf-php.git"
-    install_asdf_add_version "php@7.4.2" "php" "7.4.2" "PHP_WITHOUT_PEAR=yes"
-
-    install_asdf_plugin "halcyon/asdf-java" "java" "https://github.com/halcyon/asdf-java.git"
-    install_asdf_add_version "java@8" "java" "adopt-openjdk-8u242-b08_openj9-0.18.1"
 }
 
 main
--- src/os/install/ubuntu/work.sh
+++ src/os/install/ubuntu/work.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "../../utils.sh" \
@@ -5,34 +6,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 
 print_in_purple "\n   Work\n\n"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
-
-if ! package_is_installed "sublime-text-installer"; then
-
-    add_key "https://download.sublimetext.com/sublimehq-pub.gpg" \
-        || print_error "Sublime text (add key)"
-
-    add_to_source_list "https://download.sublimetext.com/ apt/stable/" "sublime-text.list" \
-        || print_error "Sublime text (add to package resource list)"
-
-    update &> /dev/null \
-        || print_error "Sublime text (resync package index files)"
-
-fi
-
-install_package "" "apt-transport-https"
-install_package "Sublime text" "sublime-text"
-
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
-
-install_snap "VSCode" "code" "--classic"
-execute "cat ../../../vscode/extensions.list | tr \\n \\0 | xargs -0 -n 1 code --install-extension" "Extensions VSCode"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -51,18 +24,3 @@ install_snap "Spotify" "spotify"
 printf "\n"
 
 install_snap "Slack" "slack" "--classic"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
-
-install_package "MySql" "mysql-server"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-printf "\n"
-
-install_package "Apache" "apache2"
-install_package "PHP - plugins" "libapache2-mod-php php-mcrypt php-mysql php-common php-curl php-mcrypt php-mysql php-xdebug php-xml php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-opcache php7.0-readline php7.0-xml"
-
-
