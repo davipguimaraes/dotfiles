84e60b4b8ce836d21182eae910d4db1210c5aa4f
Davi P. Guimarães <davipuiaraes.dev@gmail.com>
2020-02-26 11:44:28 -0300

    instalation of appearence themes ubuntu

---
+++ src/os/install/ubuntu/appearance.sh
@@ -0,0 +1,41 @@
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "utils.sh"
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+printf "\n"
+
+if ! package_is_installed "flat-remix-gnome"; then
+
+    add_ppa "ppa:daniruiz/flat-remix" \
+        || print_error "Flat remix (add PPA)"
+
+    update &> /dev/null \
+        || print_error "Flat remix (resync package index files)"
+
+fi
+
+install_package "Theme - Flat remix" "flat-remix-gnome"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+printf "\n"
+
+if ! package_is_installed "adapta-gtk-theme"; then
+
+    add_ppa "ppa:tista/adapta" \
+        || print_error "Flat remix (add PPA)"
+
+    update &> /dev/null \
+        || print_error "Flat remix (resync package index files)"
+
+fi
+
+install_package "Theme - Adapta" "adapta-gtk-theme"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_package "Gnome Tweak Tool" "gnome-tweak-tool"
